83e7f070123d57c62546d82c848e4f8d8a2a782c
Oksana Shadura <oksana.shadura@cern.ch>
2019-07-11 16:25:58 +0200

    pfff

--- cmake/modules/FlameGraph.cmake
+++ cmake/modules/FlameGraph.cmake
@@ -1,11 +1,19 @@
-include(ExternalProject)
 
-ExternalProject_Add(FlameGraph
-	GIT_REPOSITORY "https://github.com/brendangregg/FlameGraph.git"
-	UPDATE_COMMAND ""
-	PATCH_COMMAND ""
-	CONFIGURE_COMMAND ""
-	BUILD_COMMAND ""
-	INSTALL_COMMAND ""
-	EXCLUDE_FROM_ALL 1
-)
+set (FLAMEGRAPH_DIR ${CMAKE_BINARY_DIR}/contrib/FlameGraph)
+if (NOT EXISTS ${FLAMEGRAPH_DIR}/flamegraph.pl)
+	message (STATUS "Flamegraph not found. Downloading it!")
+	include (ExternalProject)
+	ExternalProject_Add (FlameGraph
+		UPDATE_COMMAND ""
+		SOURCE_DIR ${FLAMEGRAPH_DIR}
+		GIT_REPOSITORY https://github.com/brendangregg/FlameGraph
+		GIT_PROGRESS 1
+		GIT_SHALLOW 1
+		UPDATE_COMMAND ""
+		CONFIGURE_COMMAND ""
+		BUILD_COMMAND ""
+		INSTALL_COMMAND ""
+	)
+else ()
+	message (STATUS "Using flamegraph.pl found at ${FLAMEGRAPH_DIR}")
+endif ()
